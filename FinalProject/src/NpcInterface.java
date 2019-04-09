ed4f4c6433130c008b5a8587d2f659441b53b1e5
Erik Bjorngaard <erikbjorngaard416@hotmail.com>
2019-04-09 11:07:24 -0500

    Rudimentary item interface is completed.

---
+++ FinalProject/src/NpcInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xg6856vd
+ */
+public interface NpcInterface {
+    
+    public int getStr();
+    public void setStr(int str);
+    public int getDef();
+    public void setDef(int def);
+    public int getHp();
+    public void setHp(int hp);
+    
+}
