4a14771a6c2db6193e1db8ca6be35d7dbed01036
Erik Bjorngaard <erikbjorngaard416@hotmail.com>
2019-04-20 12:08:55 -0500

    Rudimentary Classes and Interfaces added

---
+++ FinalProject/src/Interfaces/LocationInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+/**
+ *
+ * @author xg6856vd
+ */
+public interface LocationInterface {
+    
+    public String name();
+    public String description();
+    public EventInterface addEvent();
+}
