addaa3d30a374908e6df4a91969d87b5393832f1
ajaybasnyat <ajaybasnyat@gmail.com>
2019-05-01 22:58:04 -0500

    Game Over implementation

--- FinalProject/src/GUI/CombatController.java
+++ FinalProject/src/GUI/CombatController.java
@@ -28,7 +28,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class CombatController implements Initializable {
-    
+
     @FXML
     private Label playerLabel;
 
@@ -43,28 +43,28 @@ public class CombatController implements Initializable {
 
     @FXML
     private Label enemyHealthLabel;
-    
+
     @FXML
     private Label enemyLabel;
-    
+
     @FXML
     private TextArea combatTextArea;
-    
+
     @FXML
     private Button attackButton;
-    
+
     @FXML
     private Pane enemyPane;
-    
+
     private Enemy enemy;
     private int maxHp;
     private String description;
     private boolean hasKey;
-    
-    public CombatController(){
-        
+
+    public CombatController() {
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
@@ -73,189 +73,194 @@ public class CombatController implements Initializable {
         //name 
         playerLabel.setText(hero.getName());
         enemyLabel.setText(enemy.getName());
-        
+
         //set enemy image
         setBackground();
 
         //health bar
         healthBar(hero);
         healthBar(enemy);
-        
+
         combatTextArea.appendText(description + "\n");
-        
+
     }
-    
+
     @FXML
-    public void healthBar(NpcInterface npc){
+    public void healthBar(NpcInterface npc) {
         //health bar
-        float percentage = ((float)npc.getHp() / (float)npc.maxHp());
+        float percentage = ((float) npc.getHp() / (float) npc.maxHp());
         float width = (percentage * 277);
-        if(npc.equals(hero)){
+        if (npc.equals(hero)) {
             playerHealthBar.setWidth(width);
-            if(percentage >= 0.5){
+            if (percentage >= 0.5) {
                 playerHealthBar.setFill(Color.web("#34da3c"));
-            }
-            else if(percentage >= 0.25 && percentage < 0.5){
+            } else if (percentage >= 0.25 && percentage < 0.5) {
                 playerHealthBar.setFill(Color.YELLOW);
-            }
-            else{
+            } else {
                 playerHealthBar.setFill(Color.RED);
             }
             playerHealthLabel.setText(String.valueOf(npc.getHp()) + "/" + String.valueOf(npc.maxHp()));
-        }
-        else{
-            width = (((float)npc.getHp() / (float)maxHp) * 277);
-             enemyHealthBar.setWidth(width);
-            if(percentage >= 0.5){
+        } else {
+            width = (((float) npc.getHp() / (float) maxHp) * 277);
+            enemyHealthBar.setWidth(width);
+            if (percentage >= 0.5) {
                 enemyHealthBar.setFill(Color.web("#34da3c"));
-            }
-            else if(percentage >= 0.25 && percentage < 0.5){
+            } else if (percentage >= 0.25 && percentage < 0.5) {
                 enemyHealthBar.setFill(Color.YELLOW);
-            }
-            else{
+            } else {
                 enemyHealthBar.setFill(Color.RED);
             }
             enemyHealthLabel.setText(String.valueOf(npc.getHp()) + "/" + String.valueOf(maxHp));
-            
+
         }
     }
-    
+
     @FXML
-    public void attack(ActionEvent event) throws IOException{
+    public void attack(ActionEvent event) throws IOException {
         int damage = hero.attack(enemy);
         combatTextArea.appendText("\nYou dealt " + damage + " damage to the " + enemy.getName() + ".");
-            if (!enemy.isKilled()) {
-                damage = enemy.attack(hero);
-                combatTextArea.appendText("\nThe " + enemy.getName() + " dealt " + damage + 
-                " damage to the " + hero.getName() + ".\n");
-            }
-            healthBar(hero);
-            healthBar(enemy);
-            
-            //enemy is dead
-            if(enemy.isKilled()){
-                combatTextArea.appendText("\nYou defeated the " + enemy.getName() + ".");
-                attackButton.setDisable(true);
+        if (!enemy.isKilled()) {
+            damage = enemy.attack(hero);
+            combatTextArea.appendText("\nThe " + enemy.getName() + " dealt " + damage
+                    + " damage to the " + hero.getName() + ".\n");
+        }
+        healthBar(hero);
+        healthBar(enemy);
 
-                if (chance(25)) {
-                    //Enemy drops random item from current tier - based on difficulty multiplier
-                    Random rng = new Random();
-                    ArrayList<ItemInterface> loot = items.tier(multiplier);
-                    int modifier = (rng.nextInt(loot.size()));
-                    ItemInterface item = loot.get(modifier);
+        //enemy is dead
+        if (enemy.isKilled()) {
+            combatTextArea.appendText("\nYou defeated the " + enemy.getName() + ".");
+            attackButton.setDisable(true);
 
-                    //Add item to player's inventory and remove from global item list
-                    // ensures items are unique
-                    hero.addToInventory(item);
-                    combatTextArea.appendText("\nThe " + enemy.getName() + " dropped a " + item.getName()
-                            + ".");
-                    items.removeItem(item);
-                }
-                
-                if(hasKey){
-                    combatTextArea.appendText("\nYou found a key.");
-                    multiplier++;
-                    decision.hasKey(false);
-                }
+            if (chance(25)) {
+                //Enemy drops random item from current tier - based on difficulty multiplier
+                Random rng = new Random();
+                ArrayList<ItemInterface> loot = items.tier(multiplier);
+                int modifier = (rng.nextInt(loot.size()));
+                ItemInterface item = loot.get(modifier);
+
+                //Add item to player's inventory and remove from global item list
+                // ensures items are unique
+                hero.addToInventory(item);
+                combatTextArea.appendText("\nThe " + enemy.getName() + " dropped a " + item.getName()
+                        + ".");
+                items.removeItem(item);
             }
-    }
-    
-    @FXML
-    public void flee(ActionEvent event) throws IOException{
-        Parent tableViewParent = FXMLLoader.load(getClass().getResource("Game.fxml"));
+
+            if (hasKey) {
+                combatTextArea.appendText("\nYou found a key.");
+                multiplier++;
+                decision.hasKey(false);
+            }
+        }
+
+        //hero is dead
+        if (hero.isKilled()) {
+            Parent tableViewParent = FXMLLoader.load(getClass().getResource("GameOver.fxml"));
             Scene tableViewScene = new Scene(tableViewParent);
 
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            window.setTitle("Game");
 
             window.setScene(tableViewScene);
-            window.setResizable(false);
             window.show();
+        }
     }
-    
-    
+
+    @FXML
+    public void flee(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("Game.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setTitle("Game");
+
+        window.setScene(tableViewScene);
+        window.setResizable(false);
+        window.show();
+    }
+
     public void appendMessage(String message) {
         combatTextArea.appendText(message);
     }
-    
-    public void setBackground(){
-        
+
+    public void setBackground() {
+
         enemyPane.getStyleClass().clear();
-        
+
         //set enemy background image
-        if(enemy.getName().equals("Cyclops")){
+        if (enemy.getName().equals("Cyclops")) {
             enemyPane.getStyleClass().add("cyclops");
         }
-        
-        if(enemy.getName().equals("Dragon")){
+
+        if (enemy.getName().equals("Dragon")) {
             enemyPane.getStyleClass().add("dragon");
         }
-        
-        if(enemy.getName().equals("Goblin")){
+
+        if (enemy.getName().equals("Goblin")) {
             enemyPane.getStyleClass().add("goblin");
         }
-        
-        if(enemy.getName().equals("Gryffin")){
+
+        if (enemy.getName().equals("Gryffin")) {
             enemyPane.getStyleClass().add("gryffin");
         }
-        
-        if(enemy.getName().equals("Harpy")){
+
+        if (enemy.getName().equals("Harpy")) {
             enemyPane.getStyleClass().add("harpy");
         }
-        
-        if(enemy.getName().equals("Kobold")){
+
+        if (enemy.getName().equals("Kobold")) {
             enemyPane.getStyleClass().add("kobold");
         }
-        
-        if(enemy.getName().equals("Minotaur")){
+
+        if (enemy.getName().equals("Minotaur")) {
             enemyPane.getStyleClass().add("minotaur");
         }
-        
-        if(enemy.getName().equals("Ogre")){
+
+        if (enemy.getName().equals("Ogre")) {
             enemyPane.getStyleClass().add("ogre");
         }
-        
-        if(enemy.getName().equals("Slime")){
+
+        if (enemy.getName().equals("Slime")) {
             enemyPane.getStyleClass().add("slime");
         }
-        
-        if(enemy.getName().equals("Troll")){
+
+        if (enemy.getName().equals("Troll")) {
             enemyPane.getStyleClass().add("troll");
         }
-        
-        if(enemy.getName().equals("Unicorn")){
+
+        if (enemy.getName().equals("Unicorn")) {
             enemyPane.getStyleClass().add("unicorn");
         }
-        
-        if(enemy.getName().equals("Vampire")){
+
+        if (enemy.getName().equals("Vampire")) {
             enemyPane.getStyleClass().add("vampire");
         }
-        
-        if(enemy.getName().equals("Wraith")){
+
+        if (enemy.getName().equals("Wraith")) {
             enemyPane.getStyleClass().add("wraith");
         }
-        
-        if(enemy.getName().equals("Wyvern")){
+
+        if (enemy.getName().equals("Wyvern")) {
             enemyPane.getStyleClass().add("wyvern");
         }
-        
-        if(enemy.getName().equals("Zombie")){
+
+        if (enemy.getName().equals("Zombie")) {
             enemyPane.getStyleClass().add("zombie");
         }
     }
-    
-    public void parseEvent(){
+
+    public void parseEvent() {
         description = decision.description();
         hasKey = decision.hasKey();
         enemy = decision.getEnemy();
-        
+
         //apply multiplier - might need to rework this
-        if(enemy.getHp() / multiplier != enemy.maxHp()){
+        if (enemy.getHp() / multiplier != enemy.maxHp()) {
             enemy.setDef(enemy.getDef() * multiplier);
             enemy.setStr(enemy.getStr() * multiplier);
-            enemy.setHp(enemy.getHp() * multiplier); 
+            enemy.setHp(enemy.getHp() * multiplier);
         }
-        
+
         maxHp = enemy.maxHp() * multiplier;
     }
 }
---
+++ FinalProject/src/GUI/GameOverController.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ajay
+ */
+public class GameOverController implements Initializable {
+
+    @FXML
+    private Label title;
+    @FXML
+    private Label title1;
+    @FXML
+    private Label newGame;
+    @FXML
+    private Label mainMenu;
+    @FXML
+    private Label exit;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void startNewGame(MouseEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("GameSetup.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setTitle("Game Setup");
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
+    @FXML
+    private void returnToMenu(MouseEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
+            Scene tableViewScene = new Scene(tableViewParent);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(tableViewScene);
+            window.setTitle("Main Menu");
+            
+            window.show();
+    }
+    
+    @FXML
+    private void exit(MouseEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void shrinkStart(MouseEvent event) {
+        newGame.setStyle("-fx-font: 32 System;");
+    }
+
+    @FXML
+    private void enlargeStart(MouseEvent event) {
+        newGame.setStyle("-fx-font: 42 System;");
+    }
+    
+    @FXML
+    private void shrinkMenu(MouseEvent event) {
+        mainMenu.setStyle("-fx-font: 32 System;");
+    }
+
+    @FXML
+    private void enlargeMenu(MouseEvent event) {
+        mainMenu.setStyle("-fx-font: 42 System;");
+    }
+    
+    @FXML
+    private void shrinkExit(MouseEvent event) {
+        exit.setStyle("-fx-font: 32 System;");
+    }
+
+    @FXML
+    private void enlargeExit(MouseEvent event) {
+        exit.setStyle("-fx-font: 42 System;");
+    }
+
+    
+    
+}
