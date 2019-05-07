55ed06c60ef36920b4437eb18f43895516ac0fdf
ajaybasnyat <ajaybasnyat@gmail.com>
2019-05-06 21:46:32 -0500

    game won screen

--- FinalProject/src/GUI/MainApplication.java
+++ FinalProject/src/GUI/MainApplication.java
@@ -1,15 +1,25 @@
 package GUI;
 
-//package GUI;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * This is a simple RPG in which the player must find 7 keys to unlock the door
+ * to the final area.  The player must then defeat the boss in the final area to
+ * win the game.  Along the way, the player will fight monsters that may drop
+ * items that the player can equip to aid them in their quest.
+ * @author Ajay Basnyat, Erik Bjorngaard
+ */
 public class MainApplication extends Application {
 
+    /**
+     * Display the main menu screen when the application is launched.
+     * @param primaryStage The primary stage of the application.
+     * @throws Exception if specified FXML resource cannot be loaded.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
@@ -18,7 +28,11 @@ public class MainApplication extends Application {
         primaryStage.show();
     }
 
-
+    /**
+     * Launch the application with the specified arguments.
+     * @param args The user specified arguments that are used to modify how the
+     * application is launched.
+     */
     public static void main(String[] args) {
         launch(args);
     }
